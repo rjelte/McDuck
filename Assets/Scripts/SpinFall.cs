9ce474455a5dc64b1faa05da14238b84808af384
Richard Jelte <imjelte@gmail.com>
2018-06-17 00:26:30 -0600

    Swapped placeholder object for dollar bills, and physics for interaction.
    I added a collider and rigidbody to the bills, and got the feel of what it's like to interact with them in VR. With full interaction like this, I can handle about 100-120 bills really well. When it hits 200 though, things get a little shaky (at least on my PC). Started playing witha script to make the bills rotate/move as they fall, but it's still half-baked.

---
+++ Assets/Scripts/SpinFall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinFall : MonoBehaviour {
+	private Rigidbody   rigidBody;
+	bool grounded = false;
+	public Transform groundCheck;
+	// Use this for initialization
+	void Start () {
+		this.rigidBody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1<< LayerMask.NameToLayer("Ground"));
+		var dir = this.rigidBody.velocity;
+		Debug.Log(string.Format("Velocity: {0}", dir.magnitude));
+ 		if ((dir != Vector3.zero)) {
+ 			transform.rotation = Quaternion.LookRotation(dir);
+		}
+	}
+}
