2464483345fdc8b43d4f33a7c298f423acbf8fa1
Pedro <rico.pedro@gmail.com>
2018-06-18 21:54:45 -0400

    Created input console to handle salary selection and prefab to use.

---
+++ Assets/Scripts/McDuckConsole.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class McDuckConsole : MonoBehaviour {
+
+	public int Salary;
+
+	public Text SalaryText;
+
+	public ValueObject TargetPrefab;
+
+	public bool TryUsePartial = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		UpdateSalaryText();
+	}
+
+	private void UpdateSalaryText()
+	{
+		if (!SalaryText)
+			return;
+
+		SalaryText.text = string.Format("{0:C}", Salary);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void RaiseSalary(int amount){
+		Salary += amount;
+		UpdateSalaryText();
+	}
+
+	public void ResetSalary()
+	{
+		Salary = 0;
+		UpdateSalaryText();
+	}
+
+	public void SetTargetPrefab(ValueObject targetPrefab)
+	{
+		TargetPrefab = targetPrefab;
+	}
+
+	public void MakeItRain()
+	{
+		int totalElements = Salary / TargetPrefab.Value;
+	}
+
+	// Could be float for fractions
+	public void SpawnPrefabs(int count)
+	{
+
+	}
+}
