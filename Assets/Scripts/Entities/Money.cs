330b90170720981364fad790c252d7f85b1e1041
brandmooffin <brandmooffin@outlook.com>
2018-06-18 20:06:53 -0400

    Created money model and money chute.

---
+++ Assets/Scripts/Entities/Money.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour {
+
+    public bool IsAlive;
+    public bool IsOnGround;
+
+    private const float GravityAcceleration = 3400.0f;
+    private const float MaxFallSpeed = 550.0f;
+
+    private Vector2 basePosition;
+    private float bounce;
+    private Vector2 velocity;
+
+    // Use this for initialization
+    void Start () {
+        basePosition = transform.localPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private bool CheckIfFell()
+    {
+        return transform.localPosition.y > 854 || transform.localPosition.y < -200;
+    }
+    
+    public void UpdateMoney()
+    {
+        if (CheckIfFell() && IsAlive)
+        {
+            IsAlive = false;
+        }
+
+        if (IsAlive)
+        {
+            
+        }
+    }
+
+    public void Reset(Vector3 resetPosition)
+    {
+        IsAlive = true;
+        IsOnGround = false;
+        transform.localPosition = resetPosition;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Table")
+        {
+            IsOnGround = true;
+        }
+    }
+}
---
+++ Assets/Scripts/MoneyChuteController.cs
@@ -0,0 +1,64 @@
+﻿using Assets.Scripts.Entities;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyChuteController : MonoBehaviour {
+
+    public Money Dollar;
+    public int InitialSpawnTimer;
+    public int MaxMoneyCount;
+
+    private List<Money> Money = new List<Money>();
+
+    private int spawnTimer;
+
+    // Use this for initialization
+    void Start () {
+        spawnTimer = InitialSpawnTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnTimer -= 1;
+
+        if (spawnTimer == 0 && Money.Count < MaxMoneyCount)
+        {
+            spawnTimer = InitialSpawnTimer;
+
+            Money gem = ResetMoney(MoneyTypes.Dollars);
+            Money.Add(gem);
+        }
+
+        List<Money> moneyToRemove = new List<Money>();
+
+        Money.ForEach(money =>
+        {
+            money.UpdateMoney();
+            if (!money.IsAlive)
+            {
+                moneyToRemove.Add(money);
+            }
+        });
+
+        foreach (var p in moneyToRemove)
+        {
+            Money.Remove(p);
+            Destroy(p.gameObject);
+        }
+    }
+
+    private Money ResetMoney(string moneyType)
+    {
+        Money money = null;
+        switch (moneyType)
+        {
+            case MoneyTypes.Dollars:
+                money = Instantiate(Dollar);
+                break;
+        }
+        money.transform.parent = transform;
+        money.transform.localPosition = new Vector3(0, -0.3f, 0);
+        money.IsAlive = true;
+        return money;
+    }
+}
