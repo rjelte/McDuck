f847724cd303d9f0b61fa79c4b5ac7b61457d9d4
Pedro <rico.pedro@gmail.com>
2018-06-19 23:22:47 -0400

    Added new console and fixed spawning mechanics

--- Assets/Scripts/MoneyChuteController.cs
+++ Assets/Scripts/MoneyChuteController.cs
@@ -9,6 +9,7 @@ public class MoneyChuteController : MonoBehaviour {
     public int MaxMoneyCount;
     public bool SpawnMoney;
     public bool VacuumMoney;
+	public int TotalToSpawn;
 
     private List<Money> Money = new List<Money>();
 
@@ -26,7 +27,7 @@ public class MoneyChuteController : MonoBehaviour {
 	void Update () {
         
 
-        if (SpawnMoney)
+        if (SpawnMoney && TotalToSpawn > 0)
         {
             DropMoney();
         }
@@ -79,6 +80,7 @@ public class MoneyChuteController : MonoBehaviour {
 
         if (spawnTimer <= 0 && Money.Count < MaxMoneyCount)
         {
+			TotalToSpawn--;
             spawnTimer = InitialSpawnTimer;
 
             Money gem = ResetMoney();
